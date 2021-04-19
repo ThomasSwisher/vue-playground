143a18578b15e6b4e45130b38fcd2ea05b1160e6
Thomas Swisher <thomas@structuredseo.com>
2021-04-19 16:32:05 -0600

    Final Update

--- src/components/ConditionalRenderingExercise.vue
+++ src/components/ConditionalRenderingExercise.vue
@@ -10,7 +10,7 @@
             screen.
           </p>
           <!-- add code to element below -->
-          <div class="player-card text-center">
+          <div class="player-card text-center" v-if="true==false">
             <div>
               <img :src="state.player.photo" />
             </div>
@@ -38,7 +38,7 @@
             toggle the value to true to get it to show up again.
           </p>
           <!-- add code to element below -->
-          <div class="player-card text-center">
+          <div class="player-card text-center" v-if="state.showPlayer">
             <div>
               <img :src="state.player.photo" />
             </div>
@@ -67,23 +67,23 @@
             to add another condition.
           </p>
           <!-- v-if comparing grade and 90 -->
-          <div>
+          <div v-if="state.grade >= 90">
             <p>The grade is 'A'.</p>
           </div>
           <!-- v-else-if comparing grade and 80 -->
-          <div>
+          <div v-else-if="state.grade >= 80">
             <p>The grade is 'B'.</p>
           </div>
           <!-- v-else-if comparing grade and 70 -->
-          <div>
+          <div v-else-if="state.grade >= 70">
             <p>The grade is 'C'.</p>
           </div>
           <!-- v-else-if comparing grade and 60 -->
-          <div>
+          <div v-else-if="state.grade >= 60">
             <p>The grade is 'D'.</p>
           </div>
           <!-- v-else to display if all the others fail -->
-          <div>
+          <div v-else="state.grade >= 50">
             <p>The grade is 'F'.</p>
           </div>
         </div>
@@ -122,38 +122,40 @@
 
 
 <script>
-import { reactive } from "vue";
+  import { reactive } from "vue";
 
-export default {
-  name: "condtional-rendering-exercise",
-  setup() {
-    const state = reactive({
-      //add a property to toggle the player-card here.
-      //add a property to set the number value here.
-      player: {
-        photo: "https://robohash.org/D$",
-        name: "D$",
-        position: "WR",
-        number: 4,
-      },
-    });
-    return {
-      state,
-    };
-  },
-};
+  export default {
+    name: "condtional-rendering-exercise",
+    setup() {
+      const state = reactive({
+        //add a property to toggle the player-card here.
+        showPlayer: true,
+        //add a property to set the number value here.
+        grade: 50,
+        player: {
+          photo: "https://robohash.org/D$",
+          name: "D$",
+          position: "WR",
+          number: 4,
+        },
+      });
+      return {
+        state,
+      };
+    },
+  };
 </script>
 
 
 <style scoped>
-.player-card {
-  border: 1px solid rgba(154, 154, 156, 0.4);
-  background-color: rgba(154, 154, 156, 0.4);
-  margin: 1rem;
-  border-radius: 5px;
-}
+  .player-card {
+    border: 1px solid rgba(154, 154, 156, 0.4);
+    background-color: rgba(154, 154, 156, 0.4);
+    margin: 1rem;
+    border-radius: 5px;
+  }
 
-.img {
-  height: 10rem;
-}
+  .img {
+    height: 10rem;
+  }
 </style>
